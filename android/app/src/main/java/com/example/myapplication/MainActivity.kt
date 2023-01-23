e8f172da87be1b75f5c07675fd34039b2586cec4
Artur Jankowski <artur@kramsoft.pl>
2023-01-23 21:16:41 +0100

    Add request to local predict endpoint

--- android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun getPredictionListener(view: View) {
+        val textView = findViewById<TextView>(R.id.tv_prediction)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://http://localhost:8080/predict"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET,
+            url,
+            { response -> textView.text = String.format(getString(R.string.response_success), response) },
+            { textView.text = getString(R.string.response_fail) }
+        )
+
+        queue.add(stringRequest)
     }
 }
