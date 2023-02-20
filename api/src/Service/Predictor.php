53ee7805cb7526b7e82fd9fa93d3c59617df002c
Artur Jankowski <artur@kramsoft.pl>
2023-02-20 20:41:18 +0100

    Use repository service as logic was moved there

--- api/src/Service/Predictor.php
+++ api/src/Service/Predictor.php
@@ -3,13 +3,14 @@
 namespace App\Service;
 
 use App\Repository\BigQueryRepository;
+use App\Repository\OpenWeatherRepository;
 
 class Predictor
 {
-    private BigQueryRepository $repository;
-    private OpenWeather        $openWeather;
+    private BigQueryRepository    $repository;
+    private OpenWeatherRepository $openWeather;
 
-    public function __construct(BigQueryRepository $repository, OpenWeather $openWeather)
+    public function __construct(BigQueryRepository $repository, OpenWeatherRepository $openWeather)
     {
         $this->repository  = $repository;
         $this->openWeather = $openWeather;
