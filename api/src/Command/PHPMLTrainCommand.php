834cc0623733a80ade6336a99c1a272c7043fb40
Artur Jankowski <artur@kramsoft.pl>
2023-03-27 21:05:43 +0200

    Register as command

--- api/src/Command/PHPMLTrainCommand.php
+++ api/src/Command/PHPMLTrainCommand.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PHPMLTrainCommand
+class PHPMLTrainCommand extends Command
 {
     private PHPMLRepository $repository;
 
