a471cfb68c79d62556fe807622b2f9adf453f89e
Artur Jankowski <artur@kramsoft.pl>
2023-04-02 20:59:51 +0200

    Create script for preprocessing data Add mapped data which will be used for traing and evaluation

---
+++ api/vars/scripts/DataPreProcess.php
@@ -0,0 +1,33 @@
+<?php
+
+$day = 86400;
+$file = 'testing';
+//$file = 'testing';
+$json = file_get_contents(__DIR__ . "/../data/$file.json");
+
+$data = json_decode($json, true);
+
+$processed = [];
+foreach ($data as $weatherData) {
+    $processed[$weatherData['timestamp']] = $weatherData;
+}
+
+ksort($processed);
+$sorted = array_values($processed);
+
+foreach ($sorted as $key => &$weatherData) {
+    $rainGroup = [];
+    $i         = 0;
+    while (
+        array_key_exists($innKey = ++$i + $key, $sorted)
+        && $weatherData['timestamp'] + $day > $sorted[$innKey]['timestamp']
+    ) {
+        $rainGroup[] = $sorted[$innKey]['rain'];
+    }
+
+    if ($rainGroup) {
+        $weatherData['rainAvgDayAfter'] = array_sum($rainGroup) / count($rainGroup);
+    }
+}
+
+file_put_contents(__DIR__ . "/../data/mapped_$file.json", json_encode(array_values($sorted)));
